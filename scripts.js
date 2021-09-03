fce20fcd31b6d9aefe8584e37b178edcf046e137
Rex Smith <rex.smith17@gmail.com>
2021-09-03 16:30:42 -0400

    Updated the click event for numbers

--- scripts.js
+++ scripts.js
@@ -33,3 +33,33 @@ function operate(operator, a, b) {
             break;
     }
 }
+
+// Initialize the two strings that we will use with the operator
+let firstNumber = '';
+let secondNumber = '';
+let activeNumber = '';
+let expressionOutput = document.querySelector('#input-line');
+
+// Adding event listener to number buttons
+// Function for what to do when number button clicked
+
+document.querySelectorAll('.button, .number').forEach(item => {
+    item.addEventListener('click', numberClicked);
+});
+
+function numberClicked(e) {
+    activeNumber += this.textContent;
+    expressionOutput.textContent = activeNumber;
+    console.log(activeNumber);
+}
+
+// Adding event listener to operator buttons
+// Function for what to do when operator button clicked
+
+
+
+// Adding event listener to equals button
+// Function for what to do when equals button clicked
+
+// Adding event listener to clear button
+// Function for what to do when clear button clicked
